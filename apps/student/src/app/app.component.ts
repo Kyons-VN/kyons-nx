3bf9fe39ef7732959ab84cdc5eb4d62606145e17
mibi <binhhm2009@gmail.com>
2022-09-10 03:08:38 +0700

    Initial commit

---
+++ apps/student/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kyonsvn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'student';
+}
