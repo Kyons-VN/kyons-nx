fa9903f3de33aea3477b05ce66812822262eab69
mibi <binhhm2009@gmail.com>
2023-06-29 10:53:25 +0700

    Init background video

--- apps/student/src/app/presentation/layouts/full/layout-full.component.ts
+++ apps/student/src/app/presentation/layouts/full/layout-full.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'student-layout-default',
   templateUrl: './layout-full.component.html',
+  styleUrls: ['./layout-full.component.scss'],
 })
-export class LayoutFullComponent { }
+export class LayoutFullComponent {}
